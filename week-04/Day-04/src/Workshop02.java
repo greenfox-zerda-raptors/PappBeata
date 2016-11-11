93aa889be37d479249cd4d5d22c0aa69785874b2
PappBeata <pappbeata@gmail.com>
2016-11-11 15:39:18 +0100

    Correcting insults in Guess game

--- week-04/Day-04/src/Workshop02.java
+++ week-04/Day-04/src/Workshop02.java
@@ -8,7 +8,7 @@ public class Workshop02 {
 
     public static void main(String[] args) {
 
-        System.out.println("xxxxxxxxxxxxxxxxxx\nGuess my number\nxxxxxxxxxxxxxxxxxx\n \nThis will be fun!\n\nBut first add the maxium number until which I should guess: (if you give 0, the program terminates\n");
+        System.out.println("~~~~~~~~~~~~~~~~~~~~\nGUESS MY NUMBER\n~~~~~~~~~~~~~~~~~~~~\n \nThis will be fun!\n\nBut first add the maxium number until which I should guess: (press 0 to terminate)\n");
 
         //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
         // START - Defining the maximum number for the random generator
@@ -23,13 +23,13 @@ public class Workshop02 {
                 System.out.println("Hasta luego!");
                 toStep = false;
             } else if (maxnr == -666) {
-                System.out.println("Noooo, you are kidding.\nPlease give me a positive whole number, which I can still speak out! \n");
+                System.out.println("Noooo, you are kidding!\nPlease give me a positive whole number, which I can still speak out! \n");
 
-            } else if (maxnr > 1048576) {
-                System.out.println("Wow, you seem to be confident about your guessing skills, right?\nMaybe try a number of maximum 6 digits! I wanna get home today... :)\n");
+            } else if (maxnr > 1024) {
+                System.out.println("Wow, you seem to be confident about your guessing skills, right?\nBut maybe try a number of maximum 3 digits! I wanna get home today... :)\n");
 
             } else if (maxnr < 10) {
-                System.out.println("C'mon, let's make it a bit more exciting, add one more digit!\nSo the new number is:\n");
+                System.out.println("C'mon, let's make it a bit more exciting, add one more digit!\nSo the new maximum number is:\n");
 
             } else {
                 toStep = false;
@@ -42,18 +42,52 @@ public class Workshop02 {
         if (maxnr == 0) {
 
         } else {
-            System.out.println("Cool, let's start to guess!\n");
+            System.out.println("Cool, let's start to guess! I have found my number under " + maxnr + "\n");
 
             //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-            // START - Get the number to guess
+            // Get the number to guess
 
             double random = Math.random();
             number = (int) (random * (maxnr - 1) + 1);
 
             //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-            // END - Get the number to guess
+            // Guess process
 
-            guessProcess();
+            boolean toTheEnd = true;
+
+            while (toTheEnd) {
+
+                if (maxnr == 0) {
+                    toTheEnd = false;
+                }
+
+                int guess = checkInput();
+
+                if (guess == 0) {
+                    System.out.println("Hasta luego!");
+                    toTheEnd = false;
+
+                } else if (guess == -666) {
+                    System.out.println("Hey, I thought you got it!? What we need here is a whole number between zero and the maximum number " + maxnr + " given by you. Please try again! \n");
+
+                } else if (guess > maxnr) {
+                    System.out.println("You are still shocked by Trump, aren't you? We need a number less than the maximum " + maxnr + "\n");
+
+                } else if (guess < number) {
+                    System.out.println("False! My number is higher actually. \nGive me a new guess:\n");
+
+                } else if (guess > number) {
+                    System.out.println("Not exactly! Try a bit lower. \nGive me a new guess:\n");
+
+                } else if (guess == number) {
+                    System.out.println("Yesssssss, " + number + " is my number!\n");
+                    toTheEnd = false;
+
+                } else {
+                    System.out.println("What the duck happened???\n");
+                    toTheEnd = false;
+                }
+            }
         }
     }
 
@@ -67,46 +101,6 @@ public class Workshop02 {
         } catch (InputMismatchException e) {
             userInput.nextLine();
             return -666;
-        }
-    }
-    //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-    // FUNCTION - define the answer to the user input to guide him further
-
-    public static void guessProcess (){
-
-        boolean toTheEnd = true;
-
-        while (toTheEnd) {
-
-            if (maxnr == 0) {
-            toTheEnd = false;
-            }
-
-            int guess = checkInput();
-
-            if (guess == 0) {
-                System.out.println("Hasta luego!");
-                toTheEnd = false;
-
-            } else if (guess == -666) {
-                System.out.println("Hey, I thought you got it!? What we need here is a number between zero and the maximum number given by you. Please try again! \n");
-
-            } else if (guess > maxnr) {
-                System.out.println("This is a number higher than your maximum number. We need a number less than " + maxnr + "\n");
-
-            } else if (guess < number) {
-                System.out.println("Not exactly! My number is higher actually. \nGive me a new guess is:\n");
-
-            } else if (guess > number) {
-                System.out.println("Not exactly! My number is lower actually. \nGive me a new guess is:\n");
-
-            } else if (guess == number) {
-                System.out.println("Yesssssss!\n");
-                toTheEnd = false;
-
-            } else {
-                System.out.println("What the duck happened???\n");
-            }
         }
     }
 }
