f04ee87e62c59170305c85d1b1749b121c48a5b3
PappBeata <pappbeata@gmail.com>
2016-11-27 22:08:08 +0100

    Primes defined until 100 - first version

--- week-03/Day-02/src/Primes.java
+++ week-03/Day-02/src/Primes.java
@@ -1,5 +1,33 @@
-
 public class Primes {
-    
 
+    public static void main(String... args){
+
+        boolean isPrime;
+
+        System.out.println(2);
+
+        for (int i = 3; i < 101; i ++) {
+
+            isPrime = false;
+
+            for (int j = 2; j < i / 2 + 2;) {
+
+                if (i % j == 0) {
+                    break;
+                }
+
+                if (j == i / 2) {
+                    isPrime = true;
+                }
+
+                j ++;
+
+            }
+
+            if (isPrime) {
+                System.out.println(i);
+            }
+        }
+
+    }
 }
