6fe4a0768d87ac219256ae4667ea5013a1bd9abe
PappBeata <pappbeata@gmail.com>
2016-11-28 15:09:02 +0100

    fine tuning before exam

--- week-03/Day-03/src/Workshop23.java
+++ week-03/Day-03/src/Workshop23.java
@@ -2,8 +2,6 @@ public class Workshop23{
     public static void main(String... args) {
         // Create a function with the same name as PrintValues which can print integers (int) to the output
         //>PrintValues(new String[] { "first", "second", "third"});
-        System.out.println();
-
         PrintValues(new int[] { 3, 4, 5, 6, 7});
 
 
