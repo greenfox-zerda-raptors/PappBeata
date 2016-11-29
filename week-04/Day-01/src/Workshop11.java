c9a338e2f8ef58d34dcc1c55226b59f474f851e7
PappBeata <pappbeata@gmail.com>
2016-11-29 19:52:25 +0100

    W04D1Ws11

--- week-04/Day-01/src/Workshop11.java
+++ week-04/Day-01/src/Workshop11.java
@@ -4,5 +4,8 @@ public class Workshop11{
     public static void main(String... args){
         ArrayList<String> list = new ArrayList<String>(Arrays.asList("first", "second", "third", "fourth", "fifth"));
         // Print all elements of the list
+
+        System.out.println(list);
+
     }
 }
