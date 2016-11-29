0c02a3084700f488e6d6e7a92f3a28ef07f8080d
PappBeata <pappbeata@gmail.com>
2016-11-29 20:06:33 +0100

    W04D1Ws13

--- week-04/Day-01/src/Workshop13.java
+++ week-04/Day-01/src/Workshop13.java
@@ -4,5 +4,9 @@ public class Workshop13{
     public static void main(String... args){
         ArrayList<String> list = new ArrayList<String>(Arrays.asList("first", "second", "third", "fourth", "fifth"));
         // Print all elements of the list using a for loop and an Iterator
+
+        for(Iterator <String> itr = list.iterator(); itr.hasNext();) {
+            System.out.println(itr.next());
+        }
     }
 }
