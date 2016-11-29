c52602bcec04ab4b0522bd1a1aea9ed95f1fbb07
PappBeata <pappbeata@gmail.com>
2016-11-29 20:34:18 +0100

    W04D1Ws14

--- week-04/Day-01/src/Workshop14.java
+++ week-04/Day-01/src/Workshop14.java
@@ -5,6 +5,12 @@ public class Workshop14{
         ArrayList<String> al = new ArrayList<String>(Arrays.asList("kuty", "macsk", "kacs", "rók"));
         // Add "a" to every string in al. Use an iterator!
 
+        ListIterator itr = al.listIterator();
+        for(;itr.hasNext();) {
+            Object element = itr.next();
+            itr.set(element + "a");
+
+        }
         System.out.println(al);
     }
 }
