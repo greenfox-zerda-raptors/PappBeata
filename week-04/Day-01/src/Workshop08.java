49b98a58018be16f2957d1ebe4eb130cd9743992
PappBeata <pappbeata@gmail.com>
2016-11-29 19:37:27 +0100

    W04D1Ws08

--- week-04/Day-01/src/Workshop08.java
+++ week-04/Day-01/src/Workshop08.java
@@ -5,5 +5,11 @@ public class Workshop08{
         ArrayList<Integer> arrayList = new ArrayList<Integer>(Arrays.asList(1,2,3,4,5));
         // Check if the arrayList contains "7" if it contains print "Hoorray" otherwise print "Noooooo"
 
+        if (arrayList.contains(7)){
+            System.out.println("Hoorray");
+        } else {
+            System.out.println("Noooooo");
+        }
+
     }
 }
