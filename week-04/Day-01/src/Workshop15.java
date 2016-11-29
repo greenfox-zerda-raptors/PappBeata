71c053cc353e231a780a531d359af24cdca4bce0
PappBeata <pappbeata@gmail.com>
2016-11-29 20:51:57 +0100

    W04D1Ws15

--- week-04/Day-01/src/Workshop15.java
+++ week-04/Day-01/src/Workshop15.java
@@ -4,5 +4,11 @@ public class Workshop15{
     public static void main(String... args){
         ArrayList<String> al = new ArrayList<String>(Arrays.asList("1", "2", "3", "4", "5", "6", "7" ));
         // Add "a" to every string in al. Use a for(each) loop
+
+        for (String item : al){
+            al.set(al.indexOf(item), item + "a");
+
+        }
+        System.out.println(al);
     }
 }
