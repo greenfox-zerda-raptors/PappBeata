ae4095149ad1202cfae2f7afa59f68293a190802
PappBeata <pappbeata@gmail.com>
2016-11-29 19:59:46 +0100

    W04D1Ws12

--- week-04/Day-01/src/Workshop12.java
+++ week-04/Day-01/src/Workshop12.java
@@ -5,5 +5,10 @@ public class Workshop12{
         ArrayList<Integer> list = new ArrayList<Integer>(Arrays.asList(2, 4, 6, 8, 10, 12, 14, 16));
         // Check if list contains all of the following elements: 4,8,12,16
         // Print "true" if it contains all, otherwise print "false"
+
+        ArrayList<Integer> list2 = new ArrayList<Integer>(Arrays.asList(4,8,12,16));
+
+        System.out.println(list.containsAll(list2));
+
     }
 }
