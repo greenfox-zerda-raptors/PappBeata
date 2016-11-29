0c47d7f8b92cd446da5ff360a043120ea23c4bea
PappBeata <pappbeata@gmail.com>
2016-11-29 21:56:40 +0100

    W04D1Ws16

--- week-04/Day-01/src/Workshop16.java
+++ week-04/Day-01/src/Workshop16.java
@@ -2,12 +2,15 @@ import java.util.*;
 
 public class Workshop16{
     public static void main(String... args){
-        ArrayList<Integer> al = new ArrayList<Integer>(Arrays.asList(1, 2, 3, 4, 5, 6, 7 ));
+        ArrayList<Integer> al = new ArrayList<>(Arrays.asList(1, 2, 3, 4, 5, 6, 7 ));
         // Fix this code fragment! It should remove every even number from the list.
 
-        for(int element : al){
+        ListIterator itr = al.listIterator();
+
+        for(;itr.hasNext();){
+            Integer element = (Integer) itr.next();
             if(element % 2 == 0){
-                al.remove(element);
+                itr.remove();
             }
         }
         System.out.println(al);
