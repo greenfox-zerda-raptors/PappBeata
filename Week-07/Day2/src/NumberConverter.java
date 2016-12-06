6de0be312e3bc08d52b5d5193fc39628b9990723
PappBeata <pappbeata@gmail.com>
2016-12-06 11:54:56 +0100

    W07D2TDD works for IV

--- Week-07/Day2/src/NumberConverter.java
+++ Week-07/Day2/src/NumberConverter.java
@@ -3,11 +3,11 @@ public class NumberConverter {
     public static String arabicToRoman(int i) {
         String out = "";
         if (i == 4) {
-
+            out = "IV";
             for (int j = 0; j < i; j++) {
                 out += "I";
             }
-            return out;
         }
+        return out;
     }
 }
