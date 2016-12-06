d6414ba5164c69d0b2e884c14e690fec362fcb26
PappBeata <pappbeata@gmail.com>
2016-12-06 11:59:08 +0100

    W07D2TDD works until 9 Misis solution

--- Week-07/Day2/src/NumberConverter.java
+++ Week-07/Day2/src/NumberConverter.java
@@ -1,13 +1,23 @@
 public class NumberConverter {
+    public static String arabicToRoman(int arabic) {
+        StringBuilder builder = new StringBuilder();
 
-    public static String arabicToRoman(int i) {
-        String out = "";
-        if (i == 4) {
-            out = "IV";
-            for (int j = 0; j < i; j++) {
-                out += "I";
-            }
+        int remaining = arabic;
+        remaining = appendSymbols(remaining, 9, "IX", builder);
+        remaining = appendSymbols(remaining, 5, "V", builder);
+        remaining = appendSymbols(remaining, 4, "IV", builder);
+
+        for (int i = 0; i < remaining; i++) {
+            builder.append("I");
         }
-        return out;
+        return builder.toString();
+    }
+
+    public static int appendSymbols(int remaining, int number, String symbol, StringBuilder builder) {
+        if(remaining >= number){
+            builder.append(symbol);
+            remaining -= number;
+        }
+        return remaining;
     }
 }
