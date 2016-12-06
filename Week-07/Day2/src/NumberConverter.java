be8b4e2f4c23d2cf338d1a0100df7b93764274f4
PappBeata <pappbeata@gmail.com>
2016-12-06 10:08:28 +0100

    "First version for 2 numbers"

---
+++ Week-07/Day2/src/NumberConverter.java
@@ -0,0 +1,12 @@
+public class NumberConverter {
+
+    public static String arabicToRoman(int i) {
+        String out="";
+        if(i==1){
+            out="I"; }
+        else {
+            out="II";
+        }
+        return out;
+    }
+}
