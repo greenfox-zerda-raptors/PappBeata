86a9b2ac46e344b7f93f623614f88e72a199af0f
PappBeata <pappbeata@gmail.com>
2016-12-19 15:07:15 +0100

    w09d1spring html greeting

--- week-09/springstart/src/main/java/com/greenfoxacademy/springstart/controller/Greeting.java
+++ week-09/springstart/src/main/java/com/greenfoxacademy/springstart/controller/Greeting.java
@@ -7,6 +7,4 @@ import lombok.AllArgsConstructor;
 public class Greeting {
     long id;
     String content;
-
-
 }
--- week-09/springstart/src/main/java/com/greenfoxacademy/springstart/controller/HelloRESTController.java
+++ week-09/springstart/src/main/java/com/greenfoxacademy/springstart/controller/HelloRESTController.java
@@ -4,12 +4,16 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.concurrent.atomic.AtomicLong;
+
 @RestController
 public class HelloRESTController {
 
+    private AtomicLong ids = new AtomicLong();
+
     @RequestMapping(value = "/greeting", params = {"name"})
     public Greeting Greeting(@RequestParam("name") String username) {
-        return new Greeting(1, "Buenos Días " + username);
+        return new Greeting(ids.incrementAndGet(), "Buenos Días " + username);
     }
 
 }
