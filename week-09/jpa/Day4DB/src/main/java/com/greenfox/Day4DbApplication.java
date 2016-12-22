1482d1abe1dcc27a52c8299be3b39a2d9189b234
PappBeata <pappbeata@gmail.com>
2016-12-22 14:04:59 +0100

    w09d4jpa_DB WS2 ready

---
+++ week-09/jpa/Day4DB/src/main/java/com/greenfox/Day4DbApplication.java
@@ -0,0 +1,12 @@
+package com.greenfox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Day4DbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Day4DbApplication.class, args);
+	}
+}
---
+++ week-09/jpa/initial/src/main/java/hello/Application.java
@@ -0,0 +1,7 @@
+package hello;
+
+/**
+ * Created by Papp Beata Andrea on 21.12.2016.
+ */
+public class Application {
+}
