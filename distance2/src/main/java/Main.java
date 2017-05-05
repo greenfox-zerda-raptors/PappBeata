8a0eb6f4da71e8f92ce897a009cbbaac2311858a
PappBeata <pappbeata@gmail.com>
2017-05-05 14:55:33 +0200

    distance2 points list filled

--- distance2/src/main/java/Main.java
+++ distance2/src/main/java/Main.java
@@ -2,16 +2,26 @@ import com.google.common.io.Files;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
 
 public class Main {
 
     public static void main(String[] args) throws IOException {
-        File points = new File("src/main/resources/points.dat");
-        byte[] bytes = Files.toByteArray(points);
+        File pointsFile = new File("src/main/resources/points.dat");
+        byte[] bytes = Files.toByteArray(pointsFile);
+        ArrayList<Point> points = new ArrayList<>();
 
-        int firstNr = ((bytes[2]) << 8) | (bytes[3] & 0xff);
-        System.out.println(firstNr);
-
+        for (int i = 0; i < bytes.length / 4; i++) {
+            int convertedByteX;
+            int convertedByteY;
+            Point currentPoint;
+            convertedByteX = ((bytes[4 * i]) << 8) | (bytes[4 * i + 1] & 0xff);
+            convertedByteY = ((bytes[4 * i + 2]) << 8) | (bytes[4 * i + 3] & 0xff);
+            currentPoint = new Point(convertedByteX, convertedByteY);
+            points.add(currentPoint);
+        }
+        
+        System.out.println(points.get(0).toString());
 
     }
 }
--- distance2/src/main/java/Point.java
+++ distance2/src/main/java/Point.java
@@ -1,6 +1,8 @@
 import lombok.Getter;
 import lombok.Setter;
 
+import static java.lang.String.valueOf;
+
 @Getter
 @Setter
 public class Point {
@@ -20,4 +22,10 @@ public class Point {
         distance = Math.sqrt(Math.pow((point1.xCoordinate - point2.xCoordinate), 2) + Math.pow((point1.yCoordinate - point2.yCoordinate), 2));
         return distance;
     }
+
+    public String toString() {
+
+        return valueOf(this.xCoordinate).concat(" ".concat(valueOf(this.yCoordinate)));
+    }
+
 }
